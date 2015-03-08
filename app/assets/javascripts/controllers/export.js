f7835e2f2edba75aba2134c97b4d71c8679bd0e9
Ross Cooperman <rcooperman@movableink.com>
2015-03-07 21:16:38 -0500

    Beginnings of comp dragging within the frame
    Everything pretty much works except limiting dragging to the bottom of the comp

--- app/assets/javascripts/controllers/export.js
+++ app/assets/javascripts/controllers/export.js
@@ -5,6 +5,10 @@ angular.module('app').controller('ExportController', function($scope, $http, $wi
   $scope.selectedComp = null;
   $scope.showFramesets = false;
 
+  $scope.offset = 0;
+  $scope.dragging = false;
+  $scope.dragStart = [0,0];
+
   $http.get('/framesets').then(function(response) {
     $scope.framesets = response.data.framesets;
     if ($scope.framesets.length > 0) $scope.selectFrameset($scope.framesets[0]);
@@ -37,6 +41,32 @@ angular.module('app').controller('ExportController', function($scope, $http, $wi
     $scope.frameRatio = 524 / frame.width;
   }
 
+  $scope.startDrag = function($event) {
+    if ($event.button == 0) {
+      $event.preventDefault();
+      $scope.dragging = true;
+      $scope.dragStart = [$event.pageX,$event.pageY];
+      $scope.dragEnd = [$event.pageX,$event.pageY];
+    }
+  }
+
+  $scope.drag = function($event) {
+    if ($scope.dragging) {
+      $event.preventDefault();
+      $scope.dragEnd = [$event.pageX,$event.pageY];
+    }
+  }
+
+  $scope.stopDrag = function($event) {
+    if ($scope.dragging) {
+      $event.preventDefault();
+      $scope.dragging = false;
+      $scope.offset = Math.max(0, $scope.offset + ($scope.dragStart[1] - $scope.dragEnd[1]));
+      $scope.dragStart = [0,0];
+      $scope.dragEnd = [0,0];
+    }
+  }
+
   $scope.compWrapperStyle = function() {
     if (!$scope.selectedFrame || !$scope.frameRatio) return {};
 
@@ -48,9 +78,22 @@ angular.module('app').controller('ExportController', function($scope, $http, $wi
     };
   }
 
+  $scope.compStyle = function() {
+    var dragOffset = 0;
+    if ($scope.dragging) {
+      dragOffset = Math.max(0, $scope.dragStart[1] - $scope.dragEnd[1]);
+    }
+
+    return {
+      position: 'relative',
+      top: ($scope.offset + dragOffset) * -1
+    };
+  }
+
   $scope.toggleFramesetDropdown = function($event) {
     $event.stopPropagation();
     $scope.showFramesets = !$scope.showFramesets;
   }
   $(window).on('click', function() { $scope.showFramesets = false; $scope.$apply(); });
+
 });
