59d65ca6077a8ef44a725e39b6cea0e36c63f3ab
Ross Cooperman <rcooperman@movableink.com>
2015-05-02 20:31:45 -0400

    Show logged in state in the header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def avatar_url
+    default = image_url('headshot.png')
+    if logged_in?
+      "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(current_user.email.downcase)}?d=#{URI::encode(default, /\W/)}"
+    else
+      default
+    end
+  end
 end
