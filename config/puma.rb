2a02197310fa69ca126ce1f4cb125005370db5c0
Ross Cooperman <rcooperman@movableink.com>
2015-05-03 13:59:56 -0400

    Flesh out puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,13 @@
 threads 8,32
 workers 3
+
+environment 'production'
+daemonize true
+pidfile '/home/deploy/apps/inframe/shared/tmp/pids/puma.pid'
+state_path '/home/deploy/apps/inframe/shared/tmp/pids/puma.state'
+
+stdout_redirect '/home/deploy/apps/inframe/shared/log/puma.log', '/home/deploy/apps/inframe/shared/log/puma.err', true
+
+bind 'tcp://127.0.0.1:9292'
+
 preload_app!
