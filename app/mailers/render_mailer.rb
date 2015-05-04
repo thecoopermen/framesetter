8ba8f411780ab9f4f1d4321c1ed6a01f11e9a672
Ross Cooperman <rcooperman@movableink.com>
2015-05-03 21:48:21 -0400

    Update RenderMailer to send to the right address

--- app/mailers/render_mailer.rb
+++ app/mailers/render_mailer.rb
@@ -2,6 +2,6 @@ class RenderMailer < ApplicationMailer
 
   def completion_email(export)
     @export = export
-    mail to: 'foo@example.com', subject: 'Your comps are ready'
+    mail to: export.user.email, subject: 'Your comps are framed!'
   end
 end
